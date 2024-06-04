10cabc6c3883014dbb5e74fbb2b4797ff24c37d9
Kushal Lalwani <kushallalwani.kl@gmail.com>
2024-06-04 16:11:34 +0530

    Added edit and delete functionality for comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/apiError.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { Video } from "../models/video.model.js"
+import {Like} from "../models/like.model.js"
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
@@ -76,36 +77,88 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
 
-    const {content} = req.body
-    const {videoId} = req.params
+    const { content } = req.body
+    const { videoId } = req.params
 
     const video = await Video.findById(videoId);
-    if(!video){
-        throw new ApiError(404,'Video Not Found')
+    if (!video) {
+        throw new ApiError(404, 'Video Not Found')
     }
-    if(!content){
-        throw new ApiError(400,'Content required')
+    if (!content) {
+        throw new ApiError(400, 'Content required')
     }
-    
+
     const comment = await Comment.create({
         content: content,
         owner: req.body._id,
-        video:videoId
+        video: videoId
     })
 
-    if(!comment){
-        throw new ApiError(500,"Something went wrong, cannot add comment.")
+    if (!comment) {
+        throw new ApiError(500, "Something went wrong, cannot add comment.")
     }
 
-    return res.status(201).json(new ApiResponse(201,comment,"Comment Added"))
+    return res.status(201).json(new ApiResponse(201, comment, "Comment Added"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params
+    const { content } = req.body
+
+    if (!content) {
+        throw new ApiError(400, 'Content required')
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
     
+    // comment can only be edited by the owner..
+
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to edit this comment")
+    }
+    
+    const updatedComment = await Comment.findByIdAndUpdate(comment._id, {
+        $set: {
+            content: content
+        }
+    },
+    {
+        // return updated comment
+        new: true
+    })
+    
+    if(!updateComment){
+        throw new ApiError(500,"Comment can't be updated. Please Try Again")
+    }
+    
+    return res.json(new ApiResponse(200,commentId,"Comment Edited"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const {commentId} = req.params
+    
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404, "Comment not found")    
+    }
+
+    if(comment.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "You are not authorized to delete this comment")
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+    
+    // also like have to be removed from database
+    await Like.deleteMany({
+        comment : commentId
+    })
+
+    return res.json(new ApiResponse(200,commentId,"Comment Deleted"))
+    
 })
 
 export {
